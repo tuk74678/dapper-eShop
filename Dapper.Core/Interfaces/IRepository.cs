e3b1fdaa0f8f1cdb0a914ed6d5823baeeb3a3592
Leapheng Keam <leaphengkeam@gmail.com>
2025-11-28 12:06:58 -0500

    created an interface and repository for reviews

---
+++ Dapper.Core/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Dapper.Core.Interfaces;
+
+public interface IRepository<T> where T: class
+{
+    int Insert(T obj);
+    int Delete(int id);
+    int Update(T obj);
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+}
---
+++ Dapper.Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper.Core.Entities;
+using Dapper.Core.Interfaces;
+
+namespace Dapper.Infrastructure.Repositories;
+
+public class ReviewRepository: IRepository<Reviews>
+{
+    public int Insert(Reviews obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Update(Reviews obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Reviews> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Reviews GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
