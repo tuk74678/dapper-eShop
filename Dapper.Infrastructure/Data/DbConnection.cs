b62a061b470715a54d7dffce88cf67b8eacd8650
Leapheng Keam <leaphengkeam@gmail.com>
2025-11-28 12:06:21 -0500

    established connection string in Json file and projects referencing

---
+++ Dapper.Infrastructure/Data/DbConnection.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Dapper.Infrastructure.Data;
+
+public class DbConnection
+{
+    public SqlConnection GetConnection()
+    {
+        var conn = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build()
+            .GetConnectionString("eShopDb");
+        return new SqlConnection(conn);
+    }
+}
